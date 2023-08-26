1eeb587358c6620e5c9c09552c686b2a00e8aaf7
minnyres <liuthgw23@gmail.com>
2023-08-26 11:11:46 +0800

    build keepassxc 2.7.6

--- build-keepassxc.ps1
+++ build-keepassxc.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 $env:path += ";$qt_install_dir\bin;$jom_dir;$vcpkg_dir\packages\zlib_arm64-windows\bin;$msys2_dir\usr\bin"
 $env:path += ";$msys2_dir\mingw32\bin"
 
-$keepassxc_ver = "2.7.5"
+$keepassxc_ver = "2.7.6"
 $keepassxc_url = "https://github.com/keepassxreboot/keepassxc/releases/download/$keepassxc_ver/keepassxc-$keepassxc_ver-src.tar.xz"
 
 $ProgressPreference = 'SilentlyContinue'
@@ -21,6 +21,7 @@ jom install
 Set-Location  $keepassxc_install_dir
 windeployqt --release KeePassXC.exe
 Copy-Item $vcpkg_dir\installed\arm64-windows\bin\zlib1.dll .
+Copy-Item $vcpkg_dir\installed\arm64-windows\bin\minizip.dll .
 Copy-Item $vcpkg_dir\installed\arm64-windows\bin\qrencode.dll .
 Copy-Item $vcpkg_dir\installed\arm64-windows\bin\argon2.dll .
 Copy-Item $vcpkg_dir\installed\arm64-windows\bin\botan.dll .
